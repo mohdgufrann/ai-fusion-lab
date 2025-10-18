b6e2742028d75d9508dd49020dae6b0df13508e0
mohdgufrann <mohdufran7818@gmail.com>
2025-10-18 23:11:58 +0530

    Added Layout and Multi Model

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -8,8 +8,10 @@ import {
   SidebarHeader,
   
 } from "@/components/ui/sidebar"
-import { Moon, Sun } from "lucide-react"
+import { Sun, Moon } from "lucide-react"
 import { useTheme } from "next-themes";
+import { useEffect,useState  } from "react";
+
 
 export function AppSidebar() {
     const { theme, setTheme } = useTheme();
